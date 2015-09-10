76855f47d8fc896c8d74d79f56d3aed84b7c59c1
christian marienfeld <post@chrisland.de>
2015-09-10 13:55:23 +0200

    add plugin

--- www/js/app.js
+++ www/js/app.js
@@ -70,7 +70,7 @@ var _app = {
 
 	    
 		
-		window.addEventListener('orientationchange', app.doOnOrientationChange);
+		window.addEventListener('orientationchange', _app.doOnOrientationChange);
 		
     },
     doOnOrientationChange: function (){
